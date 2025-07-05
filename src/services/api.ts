8e43bd3a3fae9a02c98761a920dfaad93c16ed7a
daniyalkhawar366 <daniyalkhawar3@gmail.com>
2025-07-06 00:44:09 +0500

    backend conn

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:3001';
+const API_BASE_URL = 'https://civchange-be-production.up.railway.app';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
